dfac4fc7d40933d321f2c1ce4adc07cc1cd36533
Cuvii <i@cuvii.dev>
2025-03-27 17:21:59 +0800

    feat: add social authentication support for GitHub and Google in auth module

--- frontend/src/lib/auth/index.ts
+++ frontend/src/lib/auth/index.ts
@@ -1,3 +1,4 @@
+/* eslint-disable node/prefer-global/process */
 import { db } from '@/db'
 import { betterAuth } from 'better-auth'
 
@@ -5,4 +6,14 @@ import { drizzleAdapter } from 'better-auth/adapters/drizzle'
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, { provider: 'pg' }),
+  socialProviders: {
+    github: {
+      clientId: process.env.GITHUB_CLIENT_ID || '',
+      clientSecret: process.env.GITHUB_CLIENT_SECRET || '',
+    },
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID || '',
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
+    },
+  },
 })
