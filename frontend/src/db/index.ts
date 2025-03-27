eb643ff81495a0665a90ce9fbd6dcacd793ca858
Cuvii <i@cuvii.dev>
2025-03-27 17:10:30 +0800

    chore: add db:generate and db:prototyping commands to package.json and enhance database connection handling in index.ts

--- frontend/src/db/index.ts
+++ frontend/src/db/index.ts
@@ -3,6 +3,21 @@ import { drizzle } from 'drizzle-orm/neon-serverless'
 import ws from 'ws'
 
 neonConfig.webSocketConstructor = ws
+
 // eslint-disable-next-line node/prefer-global/process
-const pool = new Pool({ connectionString: process.env.DATABASE_URL })
+const connectionString = process.env.DATABASE_URL
+
+if (!connectionString) {
+  throw new Error('DATABASE_URL is not set')
+}
+
+neonConfig.fetchEndpoint = (host) => {
+  const [protocol, port] = host === 'localhost' ? ['http', 4444] : ['https', 443]
+  return `${protocol}://${host}:${port}/sql`
+}
+const connectionStringUrl = new URL(connectionString)
+neonConfig.useSecureWebSocket = connectionStringUrl.hostname !== 'localhost'
+neonConfig.wsProxy = host => (host === 'localhost' ? `${host}:4444/v2` : `${host}/v2`)
+
+const pool = new Pool({ connectionString })
 export const db = drizzle(pool)
