64bb7ea4d5971bfef5d7049a1252f831a2470067
Cuvii <i@cuvii.dev>
2025-03-27 13:51:57 +0800

    chore: enhance ESLint configuration and update dependencies

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,16 +1,62 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+/**
+ * customized eslint config based on https://github.com/zolplay-cn/config-monorepo/blob/v2/packages/eslint/index.ts
+ */
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+import eslintConfigPrettier from 'eslint-config-prettier'
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+import antfu from '@antfu/eslint-config'
+import { FlatCompat } from '@eslint/eslintrc'
+import nextPlugin from '@next/eslint-plugin-next'
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+import tailwindPlugin from 'eslint-plugin-tailwindcss'
 
-export default eslintConfig;
+const compat = new FlatCompat()
+
+const base = antfu({
+  formatters: true,
+  react: true,
+  stylistic: false,
+}).overrideRules({
+  'import/order': 'off',
+  'perfectionist/sort-imports': 'off',
+  'react-refresh/only-export-components': 'off',
+})
+
+base.append([eslintPluginPrettierRecommended, eslintConfigPrettier])
+
+base.append([
+  ...compat.config({
+    plugins: ['svg-jsx'],
+    rules: {
+      'svg-jsx/camel-case-colon': 'error',
+      'svg-jsx/camel-case-dash': 'error',
+      'svg-jsx/no-style-string': 'error',
+    },
+  }),
+])
+
+base.append(tailwindPlugin.configs['flat/recommended'], {
+  settings: {
+    tailwindcss: {
+      callees: ['classnames', 'clsxm', 'cn', 'tv', 'twx', 'twc'],
+      config: './packages/ui/tailwind.config.ts',
+    },
+  },
+})
+
+base.append([
+  {
+    name: 'next',
+    plugins: { '@next/next': nextPlugin },
+    rules: {
+      ...nextPlugin.configs.recommended.rules,
+      ...nextPlugin.configs['core-web-vitals'].rules,
+      '@next/next/no-duplicate-head': 'off',
+      '@next/next/no-img-element': 'error',
+      '@next/next/no-page-custom-font': 'off',
+    },
+  },
+])
+
+export default base
