1736838e4887c2e773d5a22cebd5dce5fb564e26
Cuvii <i@cuvii.dev>
2025-03-28 09:38:18 +0800

    refactor: simplify middleware by replacing session token handling with session cookie retrieval

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -1,25 +1,23 @@
 import type { NextRequest } from 'next/server'
+import { getSessionCookie } from 'better-auth/cookies'
 import { NextResponse } from 'next/server'
 // eslint-disable-next-line node/prefer-global/process
 const API_URL = process.env.NEXT_PUBLIC_API_URL || ''
 
 export function middleware(request: NextRequest) {
   const pathname = request.nextUrl.pathname
+
   if (pathname.startsWith('/api/proxy/')) {
-    const sessionToken = request.cookies.get('session_token')
-    if (!sessionToken) {
+    const sessionCookie = getSessionCookie(request, {})
+
+    if (!sessionCookie) {
       return NextResponse.json({ error: 'Unauthorized' }, { status: 401 })
     }
-    const requestHeaders = new Headers(request.headers)
-    requestHeaders.set('Authorization', `Bearer ${sessionToken.value}`)
-    requestHeaders.set('Content-Type', 'application/json')
 
     const nextUrl = new URL(pathname.replace('/api/proxy/', ''), API_URL)
-    // eslint-disable-next-line no-console
-    console.log(`rewrite to `, nextUrl.toString())
     return NextResponse.rewrite(
       nextUrl,
-      { headers: requestHeaders },
+
     )
   }
 
