72156ac14bf0f6c7ddbd6162ff06ede19f131813
Cuvii <i@cuvii.dev>
2025-03-28 09:02:57 +0800

    fix: update authClient baseURL to fallback on Vercel URL if Better Auth URL is not set

--- frontend/src/lib/auth/authClient.ts
+++ frontend/src/lib/auth/authClient.ts
@@ -4,6 +4,6 @@ import { createAuthClient } from 'better-auth/react'
 
 export const authClient = createAuthClient({
   // eslint-disable-next-line node/prefer-global/process
-  baseURL: process.env.NEXT_PUBLIC_BETTER_AUTH_URL,
+  baseURL: process.env.NEXT_PUBLIC_BETTER_AUTH_URL || process.env.NEXT_PUBLIC_VERCEL_URL,
   plugins: [nextCookies(), anonymousClient()],
 })
